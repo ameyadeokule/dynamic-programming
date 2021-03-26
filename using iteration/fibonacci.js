4c1f238b12afa145fcfb3232f159de2e36b596d4
Ameya Deokule <deokuleameya@gmail.com>
2021-03-26 21:11:35 +0530

    changed directory structure

---
+++ using iteration/fibonacci.js
@@ -0,0 +1,11 @@
+const fib = (n) => {
+  const table = Array(n+1).fill(0)
+  table[1] = 1
+  for (let i = 0; i <= n; i++) {
+    table[i + 1] += table [i]
+    !(i+2 > n+1) ? table[i + 2] += table [i] : 'do nothing'
+  }
+  return table[n];
+}
+
+console.log(fib(100));
