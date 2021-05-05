589ec45f8f637a00c92c11bd5ff86c7efe48ef9f
Ameya Deokule <deokuleameya@gmail.com>
2021-05-06 00:06:00 +0530

    entringer number completed using table but has encountered some errors

--- using iteration/entringerNumber.js
+++ using iteration/entringerNumber.js
@@ -5,10 +5,6 @@ const entringerNumber = (n, k) => {
 
 	table[0][0] = 1
 
-	for (let i = 0; i <= n; i++) {
-		table[i][0] = 0
-	}
-
 	for (let i = 1; i <= n; i++) {
 		for (let j = 1; j <= Math.min(i, k); j++)
 			table[i][j] = table[i][j - 1] + table[i - 1][i - j]
@@ -16,3 +12,5 @@ const entringerNumber = (n, k) => {
 
 	return table[n][k]
 }
+
+console.log(entringerNumber(4, 2))
